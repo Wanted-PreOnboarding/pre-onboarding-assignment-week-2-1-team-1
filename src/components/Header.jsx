e7a7454269eea6db9f5d79c9ccd16c056ab12144
MinGyu-Kang <kagrin97@gmail.com>
2022-09-06 19:22:28 +0900

    [#4] feat : 기본적인 홈화면 구성요소 골조 작성
    기본 /로 이동했을때 나오는 홈화면의 헤더,메인List,푸터를 스타일링
    없이 골조만 제작했습니다.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div>
       <img src="" alt="메인로고" />
+      <div>검색바</div>
       <nav>
-        <div>현재 상영작</div>
-        <div>상영 예정</div>
-        <div>영화 순위</div>
+        <Link to="/">현재 상영작 </Link>
+        <Link to="/">상영 예정 </Link>
+        <Link to="/">영화 순위</Link>
       </nav>
     </div>
   );
