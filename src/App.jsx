f475ea4cf22e0a03c3a16700612bd1f1c97fca42
MinGyu-Kang <kagrin97@gmail.com>
2022-09-06 23:32:12 +0900

    [#14] feat : tanstack-query를 설치후 app에 감싸주었습니다.
    tanstack-query와 devTool를 설치후에 적용했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import List from './pages/List/List';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<List />} />
-      </Routes>
-      <Footer />
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<List />} />
+        </Routes>
+        <Footer />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 }
