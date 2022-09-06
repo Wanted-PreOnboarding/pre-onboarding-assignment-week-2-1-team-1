b99b22d82b09b2ddb07056bede280ba6eba38193
unsnruu <unsnruu@gmail.com>
2022-09-07 01:06:31 +0900

    [#21] style : propsType 추가와 스타일링
    eslint 에러를 해결하고자 propsType을 추가하였습니다.
    또한 스타일링을 추가하여 기본적이 뼈대를 잡았습니다.

---
+++ src/pages/TopRated/components/MovieCard.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropType from 'prop-types';
+import styled from '@emotion/styled';
+
+export default function MovieCard({ poster_path, release_date, title, popularity, vote_average }) {
+  const IMG_URL = `https://image.tmdb.org/t/p/w200`;
+  return (
+    <Container>
+      <img src={`${IMG_URL}/${poster_path}`} alt="" />
+      <Description>
+        <Score>
+          <span>{popularity}</span>
+          <span>{vote_average}</span>
+        </Score>
+        <Title>{title}</Title>
+        <span>{release_date}</span>
+      </Description>
+    </Container>
+  );
+}
+
+MovieCard.propTypes = {
+  id: PropType.number,
+  poster_path: PropType.string,
+  adult: PropType.bool,
+  overview: PropType.string,
+  release_date: PropType.string,
+  title: PropType.string,
+  popularity: PropType.number,
+  vote_count: PropType.number,
+  vote_average: PropType.number,
+};
+
+const Container = styled.div`
+  width: 162px;
+  display: flex;
+  flex-direction: column;
+  & img {
+    border-radius: 1rem;
+  }
+`;
+const Description = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Score = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  & span {
+    margin: 0 0.5rem;
+  }
+`;
+const Title = styled.h4`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0; ;
+`;
