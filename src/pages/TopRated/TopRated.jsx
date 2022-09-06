504e2048567c6a084786dccf3ff247272959b83a
unsnruu <unsnruu@gmail.com>
2022-09-07 01:18:55 +0900

    [#21] feat : fetch 로직 추가와 MovieCard 렌더링
    서버에서 Movie 데이터를 받아오는 로직을 추가하였습니다.
    또한 MovieCard 컴포넌트를 사용해서 기본적인 렌더링을 시행하였습니다.
    임시적으로 스타일링을 추가하였습니다.

--- src/pages/TopRated/TopRated.jsx
+++ src/pages/TopRated/TopRated.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
 
-function TopRated() {
-  return <div></div>;
+import { apiBase } from '../../api/api';
+import MovieCard from './components/MovieCard';
+
+export default function TopRated() {
+  const [movies, setMovies] = useState([]);
+  const { REACT_APP_API_KEY } = process.env;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const { data } = await apiBase(`/movie/top_rated?api_key=${REACT_APP_API_KEY}`);
+      const { results } = data;
+
+      setMovies(results);
+    };
+
+    fetchMovies();
+  }, []);
+  return (
+    <Container>
+      {movies.map(
+        ({
+          id,
+          poster_path,
+          adult,
+          overview,
+          release_date,
+          title,
+          popularity,
+          vote_count,
+          vote_average,
+        }) => (
+          <MovieCard
+            key={id}
+            id={id}
+            poster_path={poster_path}
+            adult={adult}
+            overview={overview}
+            release_date={release_date}
+            title={title}
+            popularity={popularity}
+            vote_count={vote_count}
+            vote_average={vote_average}
+          />
+        )
+      )}
+    </Container>
+  );
 }
 
-export default TopRated;
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  & > div {
+    margin: 0 0.5rem;
+  }
+`;
