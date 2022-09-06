41aaeca8578f7d056af30e3a97da081a5090aa8d
MinGyu-Kang <kagrin97@gmail.com>
2022-09-06 19:06:23 +0900

    [#4] feat : 기본 Header파일 구조 작성

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <img src="" alt="메인로고" />
+      <nav>
+        <div>현재 상영작</div>
+        <div>상영 예정</div>
+        <div>영화 순위</div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
