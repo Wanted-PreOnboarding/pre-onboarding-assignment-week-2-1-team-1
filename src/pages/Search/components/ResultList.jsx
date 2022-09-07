140b38fd4e01689a589cb849054b5e6022534e2d
gomgun <zxolqs@gmail.com>
2022-09-08 02:59:02 +0900

    [#34] feat : 검색 결과 관련 컴포넌트

---
+++ src/pages/Search/components/ResultList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroller';
+
+import ResultItem from './ResultItem';
+
+function ResultList({ movies, fetchNextPage, hasNextPage, isLoading, isError, error }) {
+  if (!movies) return <p>영화를 검색해주세요.</p>;
+  if (!movies && !isLoading) return <p>일치하는 영화가 없습니다.</p>;
+  if (isError) return <p>Error {error.toString()}</p>;
+
+  // 여기서 movies 로그 찍어보면 무한으로 fetch 하려고함...
+  // console.log(movies);
+
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {movies.pages.map(page => {
+        return page.data.results.map(movie => (
+          <ResultItem
+            key={movie.id}
+            title={movie.title}
+            release_date={movie.release_date}
+            overview={movie.overview}
+            popularity={movie.popularity}
+            poster_path={movie.poster_path}
+          />
+        ));
+      })}
+    </InfiniteScroll>
+  );
+}
+
+ResultList.propTypes = {
+  movies: PropTypes.object,
+  fetchNextPage: PropTypes.func,
+  hasNextPage: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  isError: PropTypes.bool,
+  error: PropTypes.string,
+};
+
+export default ResultList;
