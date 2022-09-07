971ea0e75751e75d5d4e329373f9754ea52909ef
unsnruu <unsnruu@gmail.com>
2022-09-07 11:32:29 +0900

    [#21] feat : react-infinite-scroller와 react-query 적용
    1. react-infinite-scroller을 설치했습니다.
    2. react-infinite-scroller을 react-query에서 제공하는
    useInfiniteQuery hook을 사용하여 연결하였습니다.
    3. 이후 리팩토링을 진행하겠습니다.

--- src/pages/TopRated/TopRated.jsx
+++ src/pages/TopRated/TopRated.jsx
@@ -1,52 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import InfiniteScroller from 'react-infinite-scroller';
 
 import { apiBase } from '../../api/api';
-import MovieCard from './components/MovieCard';
+import MovieCard from '../../components/common/MovieCard';
+
+const { REACT_APP_API_KEY } = process.env;
+const initialUrl = `/movie/top_rated?api_key=${REACT_APP_API_KEY}`;
+const fetchMovies = async pageParam => {
+  const { data } = await apiBase(pageParam);
+  return data;
+};
 
 export default function TopRated() {
-  const [movies, setMovies] = useState([]);
-  const { REACT_APP_API_KEY } = process.env;
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    ['movies'],
+    ({ pageParam = initialUrl }) => fetchMovies(pageParam),
+    {
+      cacheTime: 3600,
+      staleTime: 90,
+      getNextPageParam: lastPage => {
+        const { page, total_pages } = lastPage;
+        if (page >= total_pages) return undefined;
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const { data } = await apiBase(`/movie/top_rated?api_key=${REACT_APP_API_KEY}`);
-      const { results } = data;
-
-      setMovies(results);
-    };
-
-    fetchMovies();
-  }, []);
+        const nextUrl = initialUrl + `&page=${page + 1}`;
+        return nextUrl;
+      },
+    }
+  );
+  if (!data) return <span>no data</span>;
   return (
-    <Container>
-      {movies.map(
-        ({
-          id,
-          poster_path,
-          adult,
-          overview,
-          release_date,
-          title,
-          popularity,
-          vote_count,
-          vote_average,
-        }) => (
-          <MovieCard
-            key={id}
-            id={id}
-            poster_path={poster_path}
-            adult={adult}
-            overview={overview}
-            release_date={release_date}
-            title={title}
-            popularity={popularity}
-            vote_count={vote_count}
-            vote_average={vote_average}
-          />
-        )
-      )}
-    </Container>
+    <InfiniteScroller loadMore={fetchNextPage} hasMore={hasNextPage}>
+      <Container>
+        {data.pages.map(page =>
+          page.results.map(({ id, poster_path, title }) => (
+            <MovieCard key={id} title={title} posterPath={poster_path} />
+          ))
+        )}
+      </Container>
+    </InfiniteScroller>
   );
 }
 
@@ -58,3 +51,32 @@ const Container = styled.div`
     margin: 0 0.5rem;
   }
 `;
+
+// <Container>
+//   {movies.map(
+//     ({
+//       id,
+//       poster_path,
+//       adult,
+//       overview,
+//       release_date,
+//       title,
+//       popularity,
+//       vote_count,
+//       vote_average,
+//     }) => (
+//       <MovieCard
+//         key={id}
+//         id={id}
+//         poster_path={poster_path}
+//         adult={adult}
+//         overview={overview}
+//         release_date={release_date}
+//         title={title}
+//         popularity={popularity}
+//         vote_count={vote_count}
+//         vote_average={vote_average}
+//       />
+//     )
+//   )}
+// </Container>
