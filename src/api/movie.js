aabec581082403c084618ac9bc6e5c27d3ab1ba8
kunnie-code <kearnyyoon@gmail.com>
2022-09-07 17:43:21 +0900

    [#13] refactor : movie에  Movie Class 삭제

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,17 +1,3 @@
-import { apiBase } from './api';
-
-class Movie {
-  getUpcomingMovie(pages) {
-    const page = pages.queryKey[1];
-    return apiBase.get(`/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`);
-  }
-
-  getNow_playingMovie(pages) {
-    const page = pages.queryKey[1];
-    return apiBase.get(
-      `/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=ko-KR&page=${page}`
-    );
-  }
-}
+class Movie {}
 
 export default new Movie();
