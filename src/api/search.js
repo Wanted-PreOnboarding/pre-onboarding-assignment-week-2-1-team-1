b838a9b43b1c73a428ba561c952305f4b841a5a9
gomgun <zxolqs@gmail.com>
2022-09-08 05:18:54 +0900

    [#66] feat : 언어 변경

--- src/api/search.js
+++ src/api/search.js
@@ -3,7 +3,7 @@ import { apiBase } from './api';
 class Search {
   getSearchedMovies(title) {
     return apiBase.get(`/search/movie`, {
-      params: { api_key: process.env.REACT_APP_API_KEY, query: title },
+      params: { api_key: process.env.REACT_APP_API_KEY, query: title, language: 'ko-KR' },
     });
   }
 }
